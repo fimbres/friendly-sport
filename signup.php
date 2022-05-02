61f01b72d655e4662ec768f6962528f8c3ae673d
Alexis Patiño <patino.alexis@uabc.edu.mx>
2022-05-02 00:40:05 -0700

    Creacion del formulario
    En este commit se creo lo que es el formulario para crear cuentas, los campos que se llegaron a poner fueron basados en la historia hecha por los analistas. También se agregaron las imágenes que había hecho nuestro diseñador.

--- signup.php
+++ signup.php
@@ -9,6 +9,7 @@
     <meta name="author" content="472 UABC Group" />
     <title>Crear cuenta</title>
 
+    <link rel="icon" type="image/x-icon" href="assets/FS-icono.ico" />
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/signup.css">
 </head>
@@ -23,9 +24,171 @@
                             <!-- LOGN IN FORM by Omar Dsoky -->
                             <form method="POST" class="col-md-8">
                                 <header class="head-form">
-                                        <img src="assets/img/logo.png" class="img-fluid" style="max-width: 20%;">
-                                        <h2>Registro</h2>
+                                    <img src="assets/static/LogoFS-png.png" class="img-fluid" style="max-width: 20%; padding-bottom: 30px">
+                                    <h2>Registro</h2>
                                 </header>
+                                <div class="form-row row">
+                                    <div class="form-group col-md-6">
+                                        <label class="form-label">Nombre de usuario</label>
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text" style="display: block;">
+                                                    <i class="fa fa-user-circle"></i>
+                                                </div>
+                                            </div>
+                                            <input class="form-input form-control" type="text" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label class="form-label">Dirección</label>
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text" style="display: block;">
+                                                    <i class="fa-solid fa-location-dot"></i>
+                                                </div>
+                                            </div>
+                                            <input class="form-input form-control" type="text" required>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div class="form-row row">
+                                    <div class="form-group col-md-12">
+                                        <label class="form-label">Correo electrónico</label>
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text" style="display: block;">
+                                                    <i class="fa-solid fa-envelope"></i>
+                                                </div>
+                                            </div>
+                                            <input class="form-input form-control" type="text" required>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div class="form-row row">
+                                    <div class="form-group col-md-6">
+                                        <label class="form-label">Contraseña</label>
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text" style="display: block;">
+                                                    <i class="fa-solid fa-lock"></i>
+                                                </div>
+                                            </div>
+                                            <input class="form-input form-control" type="password" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label class="form-label">Repetir contraseña</label>
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text" style="display: block;">
+                                                    <i class="fa fa-solid fa-lock"></i>
+                                                </div>
+                                            </div>
+                                            <input class="form-input form-control" type="password" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-row row">
+                                    <div class="form-group col-md-6">
+                                        <label class="form-label">Genero</label>
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text" style="display: block;">
+                                                    <i class="fa-solid fa-venus-mars"></i>
+                                                </div>
+                                            </div>
+                                            <select class="fomr-input form-control">
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label class="form-label">Edad</label>
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text" style="display: block;">
+                                                    <i class="fa-solid fa-user"></i>
+                                                </div>
+                                            </div>
+                                            <input class="form-input form-control" type="number" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h3 class="text-center pt-4" style="font-family: 'Abel', sans-serif;">
+                                        Selecciona mínimo un deporte de tu interés
+                                </h3>
+                                <div class="form-row row check-deportes pt-4">
+                                    
+                                    <div class="form-check col-3  text-center">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox">
+                                            Futbol soccer
+                                        </label>
+                                    </div>
+                                    <div class="form-check col-3 text-center">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox">
+                                            Futbol Americano
+                                        </label>
+                                    </div>
+                                    <div class="form-check col-3 text-center">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox">
+                                            Baloncesto
+                                        </label>
+                                    </div>
+                                    <div class=" form-check col-3 text-center">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox">
+                                            Tenis
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-row row pt-2">
+                                    <div class="form-check col-3  text-center">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox">
+                                            Beisbol
+                                        </label>
+                                    </div>
+                                    <div class="form-check col-3 text-center">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox">
+                                            Petanca
+                                        </label>
+                                    </div>
+                                    <div class="form-check col-3 text-center">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox">
+                                            Voleibol
+                                        </label>
+                                    </div>
+                                    <div class=" form-check col-3 text-center">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox">
+                                            Ciclismo
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-row row text-center p-2">
+                                    <div class=" form-check col-3 text-center">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox">
+                                            Senderismo
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-row row p-5 align-content-center align-items-center align-self-center">
+                                    <div class="col-6 text-center">
+                                        <button type="submit" class="btn-primary btn col-11" style="height: 50px">Registrarme</button>
+                                    </div>
+                                    <div class="col-6 text-center">
+                                        <a href="welcome.php">
+                                            <button type="button" class="btn-secondary btn col-11" style="height: 50px">Cancelar</button>
+                                        </a>
+                                    </div>
+                                </div>
                                 <!-- End Form -->
                             </form>
                         </div>
