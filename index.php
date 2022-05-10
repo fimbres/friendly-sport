ba65037cd169bfbefb86a29f41d763987cc8ea2b
verttisaurio1 <1daescalantevertti@gmail.com>
2022-05-10 12:36:49 -0700

    primer commit
    index.php creado con navbar y footer basico.

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Friendly Sport</title>
+        <link rel="icon" type="image/x-icon" href="assets/FS-icono.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="dist/css/styles.css" rel="stylesheet" />
+        <link href="dist/css/index_styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- barra de navegacion-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow my-0 py-0" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top"><img src="assets/static/LogoFS.png" height="64px" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="bi-list"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                        <li class="nav-item"><a class="boton_salir" href="#">Salir de la Sesion</a></li>
+                        <li class="nav-item"><a class="boton_perfil" href="#">Mostrar Perfil</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- contenido de la pagina-->        
+        <div class="contenido">
+            <div class="rectangulo">
+                
+
+
+            </div>
+        </div>
+        <!-- Footer-->
+        <footer class="bg-black text-center py-3">
+            <div class="container px-5">
+                <div class="text-white-50 small">
+                    <p>© FriendlySport, 2022. Todos los derechos reservados.</p>
+                    <p>Tel: 1234567890 | Email: ayuda@metafusions.com</p>
+                    <a href="#!">Privacidad</a>
+                    <span class="mx-1">&middot;</span>
+                    <a href="#!">Terminos</a>
+                    <span class="mx-1">&middot;</span>
+                    <a href="#!">Condiciones</a>
+                </div>
+            </div>
+        </footer>
+
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
