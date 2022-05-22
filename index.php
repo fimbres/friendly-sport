fe04bd253f5ac726ec3527c6512037a2ac527256
verttisaurio1 <1daescalantevertti@gmail.com>
2022-05-22 14:26:42 -0700

    fix: caso de no eventos
    se cambio un if para que en caso de que no haya eventos aparesca apropiadamente la lupa

--- index.php
+++ index.php
@@ -67,9 +67,6 @@
             $senderismo = [];
 
             $max = 15;
-
-            $query = "SELECT * FROM tb_evento";
-            $res = mysqli_query($BD,$query);
             
             $query_soccer = "SELECT * FROM tb_evento LEFT JOIN tb_relacion_deportes_eventos ON tb_evento.id_evento = tb_relacion_deportes_eventos.id_evento WHERE tb_relacion_deportes_eventos.id_deporte =1 ORDER BY fecha";
             $pre_soccer = mysqli_query($BD,$query_soccer);
@@ -166,7 +163,7 @@
                 </div>
             </div>
             <div class="division2">
-                <?php if ($res):?>
+                <?php if ($soccer || $americano || $baloncesto || $tenis || $beisbol || $petanca || $voleibol || $ciclismo || $senderismo):?>
                     <!-- SOCCER-->   
                     <?php if ($soccer):{?>
                     <div class="soccer">
